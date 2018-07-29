edef148473fd202c713b161c54ef0fe6d52ebebb
Omar <omar2535@gmail.com>
2018-07-28 22:01:26 -0700

    Completed on/off states for extension

--- contentScript.js
+++ contentScript.js
@@ -1,17 +1,19 @@
 $(document).ready(function(){
     
-    chrome.storage.local.get(['replacementText', 'replacementURL', 'replacementImageURL'], function (result) {
+    chrome.storage.local.get(['replacementText', 'replacementURL', 'replacementImageURL', 'state'], function (result) {
         console.log(result);
         //Replace all text
-        $('body :not(script)').contents().filter(function () {
-            return this.nodeType === 3;
-        }).replaceWith(function () {
-            return this.nodeValue.replace(/e/g, result.replacementText);
-        });
-        //replace all images
-        $("img").attr("src", result.replacementImageURL);
-        //replace all links(hrefs)
-        $("a").attr("href", result.replacementURL);
+        if(result.state){
+            $('body :not(script)').contents().filter(function () {
+                return this.nodeType === 3;
+            }).replaceWith(function () {
+                return this.nodeValue.replace(/e/g, result.replacementText);
+            });
+            //replace all images
+            $("img").attr("src", result.replacementImageURL);
+            //replace all links(hrefs)
+            $("a").attr("href", result.replacementURL);
+        }
     });
 
 });
